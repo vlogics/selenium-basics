139b5804dc9bff22bd7bc17f14258e8b9e80e189
Sashi <sasiatecs@gmail.com>
2017-07-08 15:54:45 +0530

    First program

---
+++ src/com/vlogics/selenium/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.vlogics.selenium;
+
+/**
+ * Created by sys on 7/8/2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("This is my first program in intelliJ");
+    }
+}
