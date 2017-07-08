bcbe595a7b6b4dfc250e5f9b8cb92c9c18b8ea6f
Sashi <sasiatecs@gmail.com>
2017-07-08 16:02:56 +0530

    First program

--- src/com/vlogics/selenium/HelloWorld.java
+++ src/com/vlogics/selenium/HelloWorld.java
@@ -6,5 +6,6 @@ package com.vlogics.selenium;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("This is my first program in intelliJ");
+        System.out.println("This is my second line.");
     }
 }
